380fbe774d67851d7a8a34adbcf55485ffe9b941
alfo0924 <halo0924@yahoo.com.tw>
2024-12-19 17:40:52 +0800

    v1.0137 initial upload

--- src/test/java/org/example/cucumberTest/RunCucumberTest.java
+++ src/test/java/org/example/cucumberTest/RunCucumberTest.java
@@ -1,14 +1,17 @@
 package org.example.cucumberTest;
 
+import org.junit.platform.suite.api.ConfigurationParameter;
+import org.junit.platform.suite.api.IncludeEngines;
+import org.junit.platform.suite.api.SelectClasspathResource;
+import org.junit.platform.suite.api.Suite;
 
-import io.cucumber.junit.platform.engine.Cucumber;
-import org.junit.runner.RunWith;
+import static io.cucumber.junit.platform.engine.Constants.GLUE_PROPERTY_NAME;
+import static io.cucumber.junit.platform.engine.Constants.PLUGIN_PROPERTY_NAME;
 
-@RunWith(Cucumber.class)
-@CucumberOptions(
-        features = "src/test/resources/features",
-        glue = "org.example.cucumberTest.steps",
-        plugin = {"pretty", "html:target/cucumber-reports"}
-)
+@Suite
+@IncludeEngines("cucumber")
+@SelectClasspathResource("features")
+@ConfigurationParameter(key = GLUE_PROPERTY_NAME, value = "org.example.cucumberTest.steps")
+@ConfigurationParameter(key = PLUGIN_PROPERTY_NAME, value = "pretty, html:target/cucumber-reports")
 public class RunCucumberTest {
 }
